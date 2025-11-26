6fc35c7c41d3ad1c1ab7f925560e3f075ac24e9b
Vivekdharan Muthupandi <vivekmuthupandi@gmail.com>
2025-11-26 09:22:03 +0530

    Update ChatMessage.java

--- src/main/java/com/healthcare_application/HealthCare/chat/entity/ChatMessage.java
+++ src/main/java/com/healthcare_application/HealthCare/chat/entity/ChatMessage.java
@@ -1,14 +1,4 @@
-package com.healthcare_application.HealthCare.chat.entity;
 
-import java.time.LocalDateTime;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import lombok.Getter;
-import lombok.Setter;
 
 @Entity
 @Table(name = "chat_messages")
