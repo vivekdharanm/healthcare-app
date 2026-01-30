55014f21591fa8abf27bc4fa65129acf59eae5fd
Vivekdharan Muthupandi <vivekmuthupandi@gmail.com>
2026-01-30 11:27:37 +0530

    Update HealthCareApplicationTests.java

--- src/test/java/com/healthcare_application/HealthCare/HealthCareApplicationTests.java
+++ src/test/java/com/healthcare_application/HealthCare/HealthCareApplicationTests.java
@@ -4,7 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class HealthCareApplicationTests {
+class HealthCareApplicationTests 
+{
 
 	@Test
 	void contextLoads() {
